848c1fae7d3ae926f14b713d99f3b9770dc8336e
0xffhh <henri@falconforce.nl>
2023-04-14 21:06:37 +0200

    Small improvements

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Disabled for testing
-# set -e
+set -e
 
+# The commented lines below are in place to easily switch to the official AzureHound repo once the PR
+# to add this functionality is merged.
 cd /tmp
 git clone -b hh-fork/feat-az-managed-id https://github.com/0xffhh/AzureHound.git
 # Once the PR is merged and a new build is made, you can use the main repo. 
@@ -11,6 +12,6 @@ cd /tmp/AzureHound/
 # Uncomment the two lines below when the PR to add support for System Assigned Managed Identity is merged and released.
 # git fetch --tags
 # latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
-git checkout $latestTag
+# git checkout $latestTag
 # technically, you need to build with some more flags, but doesn't really matter for now
 go build
